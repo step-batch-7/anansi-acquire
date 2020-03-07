f18315eb6c0a25adffb79d23042439bbcc8943c5
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-07 13:41:46 +0530

    |#14|Abhilash/Gulshan| Added tests for join page
    Added tests for getting join page
    Added tests for invalid game id
    Added test for valid game id
    Added test for ongoing game id

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -11,12 +11,71 @@ describe('GET', () => {
       .expect(/acquire/, done);
   });
 
-  it('should get the index.css for "/css/index.css" path', done => {
+  it('should get the index.css for "css/index.css" path', done => {
     request(app)
       .get('/css/index.css')
       .set('accept', '*/*')
       .expect(200)
       .expect('Content-Type', /text\/css/)
       .expect(/body {/, done);
+  });
+
+  it('should get the join.html for /join.html', done => {
+    request(app)
+      .get('/join.html')
+      .expect(200)
+      .expect('Content-Type', /html/, done);
+  });
+
+  it('should get the join.js for /js/join.js', done => {
+    request(app)
+      .get('/js/join.js')
+      .expect(200)
+      .expect('Content-Type', /javascript/, done);
+  });
+});
+
+describe('POST', () => {
+  describe('/joinGame', () => {
+    it('should give Game id for valid game id for path /joingame', done => {
+      const body = JSON.stringify({name: 'john', gameId: '1234'});
+      const expected = {isAnyError: false, msg: 'Game id: 1234'};
+      const expectedJson = JSON.stringify(expected);
+      app.locals.games = {1234: {noOfPlayers: 3, players: ['ram']}};
+      request(app)
+        .post('/joingame')
+        .set('Content-Type', 'application/json')
+        .send(body)
+        .expect(200)
+        .expect(expectedJson)
+        .expect('Content-Type', /json/, done);
+    });
+  });
+
+  it('should give invalid message for invalid id for /joingame', done => {
+    const body = JSON.stringify({name: 'john', gameId: '123456'});
+    const expected = JSON.stringify({isAnyError: true, msg: 'Invalid game id'});
+    app.locals.games = {1234: {noOfPlayers: 3, players: ['ram']}};
+    request(app)
+      .post('/joingame')
+      .set('Content-Type', 'application/json')
+      .send(body)
+      .expect(200)
+      .expect(expected)
+      .expect('Content-Type', /json/, done);
+  });
+
+  it('should give Game started msg for ongoing game for /joingame', done => {
+    const body = JSON.stringify({name: 'john', gameId: '1234'});
+    const expected = {isAnyError: true, msg: 'The game already started'};
+    const expectedJson = JSON.stringify(expected);
+    app.locals.games = {1234: {noOfPlayers: 3, players: ['ram', 'anu', 'sid']}};
+    request(app)
+      .post('/joingame')
+      .set('Content-Type', 'application/json')
+      .send(body)
+      .expect(200)
+      .expect(expectedJson)
+      .expect('Content-Type', /json/, done);
   });
 });
