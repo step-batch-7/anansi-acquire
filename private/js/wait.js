cc124f7ca3ff189a03fc95fa6bc68981ed00881e
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-09 13:11:48 +0530

    |#16|Abhilash/Gulshan| Showing players joined in the waiting page

--- private/js/wait.js
+++ private/js/wait.js
@@ -1,16 +1,26 @@
 const seconds = 1000;
-const displayGameId = function({isJoined, gameId}) {
+
+const generateJoined = function(players) {
+  const template = 'joined the game';
+  return players.reduce((text, player) => text + `${player} ${template}\n`, '');
+};
+
+const displayStatus = function({isJoined, gameId, hosted, joined, remaining}) {
+  const template = `hosted the game\n${generateJoined(joined)}`;
   if(isJoined) {
-    window.location = 'play.html';
+    setTimeout(() => location.replace('play.html'), 2000);
+    document.querySelector('#status').innerText = 'Starting the game';
   }
   document.querySelector('#gameId').innerText = gameId;
+  document.querySelector('#players').innerText = `${hosted} ${template}`;
+  document.querySelector('#remaining').innerText = remaining;
 };
 
-const getGameId = function() {
+const getStatus = function() {
   fetch('wait')
     .then(res => res.json())
-    .then(displayGameId);
+    .then(displayStatus);
 };
 
-getGameId();
-window.onload = () => setInterval(getGameId, seconds);
+getStatus();
+window.onload = () => setInterval(getStatus, seconds);
