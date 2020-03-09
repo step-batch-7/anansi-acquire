10c4143e24bab4c9badc6d5e1b115d75eeab08d6
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-03-09 11:07:10 +0530

    |#16|Abhilash/Gulshan| Added an interval in waiting page to check whether everyone joned

--- private/js/wait.js
+++ private/js/wait.js
@@ -1,4 +1,8 @@
-const displayGameId = function({gameId}) {
+const seconds = 1000;
+const displayGameId = function({isJoined, gameId}) {
+  if(isJoined) {
+    window.location = 'play.html';
+  }
   document.querySelector('#gameId').innerText = gameId;
 };
 
@@ -8,4 +12,5 @@ const getGameId = function() {
     .then(displayGameId);
 };
 
-window.onload = getGameId;
+getGameId();
+window.onload = () => setInterval(getGameId, seconds);
