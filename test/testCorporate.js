3a6f0fedc739cbbbf9556f287a356356d74419d6
rivu <58026024+unphydra@users.noreply.github.com>
2020-03-11 12:59:40 +0530

    |#9|Rivu/Gulshan| added corporations class
    added corporations class with method->
      establishCorporate, mergeCorporate, getInactiveCorporate
    
    modifie coporate class, and added test

---
+++ test/testCorporate.js
@@ -0,0 +1,110 @@
+const assert = require('chai').assert;
+const Corporate = require('../lib/models/corporate.js');
+
+describe('Corporate', function() {
+  describe('status', function() {
+    it('should give status of given corporation', function() {
+      const phoenix = Corporate.create('phoenix');
+      const expected = {
+        stocks: 25,
+        tiles: [],
+        area: 0,
+        price: 0,
+        majority: 0,
+        minority: 0,
+        isActive: false,
+        isStable: false
+      };
+      assert.deepStrictEqual(phoenix.status, expected);
+    });
+  });
+
+  describe('establish', () => {
+    it('should establish the corporate with the given tile and change the active status ', () => {
+      const corporate = Corporate.create('zeta');
+      corporate.establish([0, 1]);
+      const expected = {
+        stocks: 25,
+        tiles: [0, 1],
+        area: 2,
+        price: 200,
+        majority: 2000,
+        minority: 1000,
+        isActive: true,
+        isStable: false
+      };
+      assert.deepStrictEqual(corporate.status, expected);
+    });
+
+    it('should establish a stable corporate with the given tile and change the active status', () => {
+      const corporate = Corporate.create('zeta');
+      corporate.establish([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+      const expected = {
+        stocks: 25,
+        tiles: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+        area: 12,
+        price: 700,
+        majority: 7000,
+        minority: 3500,
+        isActive: true,
+        isStable: true
+      };
+      assert.deepStrictEqual(corporate.status, expected);
+    });
+  });
+
+  describe('addTile', () => {
+    it('should add tile to the given corporate ', () => {
+      const corporate = Corporate.create('zeta');
+      corporate.establish([0, 1]);
+      corporate.addTiles([2, 3]);
+      const expected = {
+        stocks: 25,
+        tiles: [0, 1, 2, 3],
+        area: 4,
+        price: 400,
+        majority: 4000,
+        minority: 2000,
+        isActive: true,
+        isStable: false
+      };
+      assert.deepStrictEqual(corporate.status, expected);
+    });
+
+    it('should add tile and make it stable when tiles are more than 11', () => {
+      const corporate = Corporate.create('zeta');
+      corporate.establish([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+      corporate.addTiles([10]);
+      const expected = {
+        stocks: 25,
+        tiles: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        area: 11,
+        price: 700,
+        majority: 7000,
+        minority: 3500,
+        isActive: true,
+        isStable: true
+      };
+      assert.deepStrictEqual(corporate.status, expected);
+    });
+  });
+
+  describe('defunct', () => {
+    it('should defunct the given corp', () => {
+      const corporate = Corporate.create('zeta');
+      corporate.establish([0, 1, 2]);
+      corporate.defunct();
+      const expected = {
+        stocks: 25,
+        tiles: [],
+        area: 0,
+        price: 0,
+        majority: 0,
+        minority: 0,
+        isActive: false,
+        isStable: false
+      };
+      assert.deepStrictEqual(corporate.status, expected);
+    });
+  });
+});
---
+++ test/testCorporations.js
@@ -0,0 +1,43 @@
+const assert = require('chai').assert;
+const Corporations = require('../lib/models/corporations.js');
+
+describe('Corporations', () => {
+  describe('establishCorporate', () => {
+    it('should establish the given corporate when it is inactive', () => {
+      const corporations = new Corporations();
+      assert.ok(corporations.establishCorporate('zeta', [13, 14]));
+    });
+
+    it('should not establish the given corporate when it is active', () => {
+      const corporations = new Corporations();
+      corporations.establishCorporate('zeta', [13, 14]);
+      assert.notOk(corporations.establishCorporate('zeta', [13, 14]));
+    });
+  });
+
+  describe('mergeCorporate', () => {
+    it('should merge the smaller to bigger corporate when smaller is not stable', () => {
+      const corporations = new Corporations();
+      corporations.establishCorporate('zeta', [12, 13, 14]);
+      corporations.establishCorporate('sackson', [16, 17]);
+      assert.ok(corporations.mergeCorporate('zeta', 'sackson', [15]));
+    });
+
+    it('should not merge the smaller to bigger corporate when smaller is stable', () => {
+      const corporations = new Corporations();
+      corporations.establishCorporate('zeta', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+      corporations.establishCorporate('sackson', [24, 36]);
+      assert.notOk(corporations.mergeCorporate('sackson', 'zeta', [12]));
+    });
+  });
+
+  describe('getInactiveCorporate', () => {
+    it('should return the inactive corporate names', () => {
+      const corporations = new Corporations();
+      corporations.establishCorporate('zeta', [12, 13, 14]);
+      corporations.establishCorporate('sackson', [16, 17]);
+      const expected = ['phoenix', 'quantum', 'hydra', 'fusion', 'america'];
+      assert.deepStrictEqual(corporations.getInactiveCorporate(), expected);
+    });
+  });
+});
